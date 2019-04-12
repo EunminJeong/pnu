02d870ec01ad09760668d04d48238c5c5b360f71
EunminJeong <jeongeunmin@gmail.com>
2019-04-12 15:19:49 +0900

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Item
+
+def item_list(request):
+    # DB로부터 모든 Item List를 가져올 예정
+    qs  = Item.objects.all()  # QuerySet 타입
+    # 템플릿 파일 위치 : shop/templates/shop/item_list.html
+    return render(request, 'shop/item_list.html', {
+        'item_list': qs,
+    })
+
+def item_detail(request, pk): 
+    item = Item.objects.get(pk=pk)
+    return render(request, 'shop/item_detail.html', {
+        'item' : item,
+    })
